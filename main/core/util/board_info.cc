ab290d3639d3ed8c3c74f8f5c5384857c2558d55
nbaiot <nbaiot@126.com>
2022-01-08 21:22:15 +0800

    add board_info
    Signed-off-by: nbaiot <nbaiot@126.com>

---
+++ main/core/util/board_info.cc
@@ -0,0 +1,30 @@
+
+#include "board_info.h"
+
+#include "sdkconfig.h"
+
+#include "esp_spi_flash.h"
+#include "esp_system.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+namespace esp {
+void PrintBoardInfoToLog() {
+  printf("############### BOARD INFO ###############\n");
+  esp_chip_info_t chip_info;
+  esp_chip_info(&chip_info);
+  printf("This is %s chip with %d CPU core(s) \n WiFi%s%s \n", CONFIG_IDF_TARGET,
+         chip_info.cores, (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+         (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+
+  printf("silicon revision %d\n", chip_info.revision);
+
+  printf(
+      "%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+      (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+  printf("Minimum free heap size: %d bytes\n",
+         esp_get_minimum_free_heap_size());
+}
+}  // namespace esp
--- main/esp_smart_home.cc
+++ main/esp_smart_home.cc
@@ -1,10 +1,13 @@
 
 #include "core/led/led_indicator_wrapper.h"
 #include "core/util/delay.h"
+#include "core/util/board_info.h"
 
 using namespace esp;
 
 extern "C" void app_main(void) {
+  PrintBoardInfoToLog();
+
   LedIndicator led(26, 0, LedIndicator::GPIO);
   led.Start(LedIndicator::CONNECTING);
   for (;;) {
